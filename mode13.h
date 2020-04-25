c727bf12cd71f25ef54906882afe4533f8e106e3
pierroelmito <pierroelmito@gmail.com>
2020-04-25 15:41:08 +0200

    clean up & format

--- mode13.h
+++ mode13.h
@@ -1,52 +1,43 @@
 #pragma once
 
-void setMode(unsigned short mode)
-{
-	asm volatile ("int $0x10\n" : : "a"(mode));
+void setMode(unsigned short mode) { asm volatile("int $0x10\n" : : "a"(mode)); }
+
+template <class T> void setPalette(const T &f) {
+  asm volatile("mov $0x3c8, %%dx\n"
+               "xor %%al, %%al\n"
+               "out %%al, %%dx\n"
+               :
+               :
+               : "dx", "ax");
+  for (int i = 0; i < 256; ++i) {
+    int r, g, b;
+    f(i, r, g, b);
+    asm volatile("mov $0x3c9, %%dx\n"
+                 "out %%al, %%dx\n"
+                 "mov %%bl, %%al\n"
+                 "out %%al, %%dx\n"
+                 "mov %%bh, %%al\n"
+                 "out %%al, %%dx\n"
+                 :
+                 : "a"(r), "b"(g | (b << 8))
+                 : "dx");
+  }
 }
 
-template <class T>
-void setPalette(const T& f)
-{
-	asm volatile (
-		"mov $0x3c8, %%dx\n"
-		"xor %%al, %%al\n"
-		"out %%al, %%dx\n"
-		:
-		:
-		: "dx", "ax"
-	);
-	for (int i = 0; i < 256; ++i) {
-		int r, g, b;
-		f(i, r, g, b);
-		asm volatile (
-			"mov $0x3c9, %%dx\n"
-			"out %%al, %%dx\n"
-			"mov %%bl, %%al\n"
-			"out %%al, %%dx\n"
-			"mov %%bh, %%al\n"
-			"out %%al, %%dx\n"
-			:
-			: "a"(r), "b"(g | (b << 8))
-			: "dx"
-		);
-	}
+void fill(unsigned char *const dst, unsigned char c, int const count) {
+  unsigned int *const idst = (unsigned int *)dst;
+  const int icount = count / 4;
+  const unsigned int tmp = c;
+  const unsigned int v = (c << 24) | (c << 16) | (c << 8) | (c << 0);
+  for (int i = 0; i < icount; ++i)
+    idst[i] = v;
 }
 
-void fill(unsigned char* const dst, unsigned char c, int const count) {
-	unsigned int* const idst = (unsigned int*)dst;
-	const int icount = count / 4;
-	const unsigned int tmp = c;
-	const unsigned int v = (c << 24) | (c << 16) |(c << 8) | (c << 0);
-	for (int i = 0; i < icount; ++i)
-		idst[i] = v;
+void blit(unsigned char *const dst, const unsigned char *const src,
+          const int count) {
+  unsigned int *const idst = (unsigned int *)dst;
+  unsigned int *const isrc = (unsigned int *)src;
+  const int icount = count / 4;
+  for (int i = 0; i < icount; ++i)
+    idst[i] = isrc[i];
 }
-
-void blit(unsigned char* const dst, const unsigned char* const src, const int count) {
-	unsigned int* const idst = (unsigned int*)dst;
-	unsigned int* const isrc = (unsigned int*)src;
-	const int icount = count / 4;
-	for (int i = 0; i < icount; ++i)
-		idst[i] = isrc[i];
-}
-
--- pipomath.h
+++ pipomath.h
@@ -3,93 +3,113 @@
 const float PI = 3.14159265359;
 const float PI_2 = PI / 2.0f;
 
-template <typename T>
-inline T slerpi(T x, T b)
-{
-	return (3 * x * x * b - 2 * x * x * x) / (b * b);
+template <typename T> inline constexpr T slerpi(T x, T b) {
+  return (3 * x * x * b - 2 * x * x * x) / (b * b);
 }
 
-template <typename T>
-inline T fakesini(T x, T b)
-{
-	const T nx = x & (b - 1);
-	const T nb = b / 2;
-	return (nx < nb) ? (slerpi<T>(nx, nb)) : (nb - 1 - slerpi<T>(nx - nb, nb));
+template <typename T> inline constexpr T fakesini(T x, T b) {
+  const T nx = x & (b - 1);
+  const T nb = b / 2;
+  return (nx < nb) ? (slerpi<T>(nx, nb)) : (nb - 1 - slerpi<T>(nx - nb, nb));
 }
 
-template <class T> inline T abs(T v) { return v > T() ? v : -v; }
-template <class T> inline T max(T a, T b) { return a > b ? a : b; }
-template <class T> inline T min(T a, T b) { return a < b ? a : b; }
-template <class T> inline T square(T x) { return x * x; }
-template <class T> inline T lengthSq(T dx, T dy) { return square(dx) + square(dy); }
-
-float Q_rsqrt(float number)
-{
-	long i;
-	float x2, y;
-	const float threehalfs = 1.5F;
-	x2 = number * 0.5F;
-	y = number;
-	i = * ( long * ) &y;
-	i = 0x5f3759df - ( i >> 1 );
-	y = * ( float * ) &i;
-	y = y * ( threehalfs - ( x2 * y * y ) );
-	return y;
+template <class T> inline constexpr T abs(T v) { return v > T() ? v : -v; }
+template <class T> inline constexpr T max(T a, T b) { return a > b ? a : b; }
+template <class T> inline constexpr T min(T a, T b) { return a < b ? a : b; }
+template <class T> inline constexpr T square(T x) { return x * x; }
+template <class T> inline constexpr T lengthSq(T dx, T dy) {
+  return square(dx) + square(dy);
 }
 
-float ApproxAtan2(float y, float x)
-{
-	const float n1 = 0.97239411f;
-	const float n2 = -0.19194795f;
-	float result = 0.0f;
-	if (x != 0.0f) {
-		const union { float flVal; unsigned int nVal; } tYSign = { y };
-		const union { float flVal; unsigned int nVal; } tXSign = { x };
-		if (abs(x) >= abs(y)) {
-			union { float flVal; unsigned int nVal; } tOffset = { PI };
-			tOffset.nVal |= tYSign.nVal & 0x80000000u;
-			tOffset.nVal *= tXSign.nVal >> 31;
-			result = tOffset.flVal;
-			const float z = y / x;
-			result += (n1 + n2 * z * z) * z;
-		} else {
-			union { float flVal; unsigned int nVal; } tOffset = { PI_2 };
-			tOffset.nVal |= tYSign.nVal & 0x80000000u;
-			result = tOffset.flVal;
-			const float z = x / y;
-			result -= (n1 + n2 * z * z) * z;
-		}
-	} else if (y > 0.0f) {
-		result = PI_2;
-	} else if (y < 0.0f) {
-		result = -PI_2;
-	}
-	return result;
+float Q_rsqrt(float number) {
+  long i;
+  float x2, y;
+  const float threehalfs = 1.5F;
+  x2 = number * 0.5F;
+  y = number;
+  i = *(long *)&y;
+  i = 0x5f3759df - (i >> 1);
+  y = *(float *)&i;
+  y = y * (threehalfs - (x2 * y * y));
+  return y;
 }
 
-template <class T>
-struct vec3
-{
-	T x {};
-	T y {};
-	T z {};
-	vec3& operator+=(const vec3<T>& o) { x += o.x; y += o.y; z += o.z; return *this; }
+float ApproxAtan2(float y, float x) {
+  const float n1 = 0.97239411f;
+  const float n2 = -0.19194795f;
+  float result = 0.0f;
+  if (x != 0.0f) {
+    const union {
+      float flVal;
+      unsigned int nVal;
+    } tYSign = {y};
+    const union {
+      float flVal;
+      unsigned int nVal;
+    } tXSign = {x};
+    if (abs(x) >= abs(y)) {
+      union {
+        float flVal;
+        unsigned int nVal;
+      } tOffset = {PI};
+      tOffset.nVal |= tYSign.nVal & 0x80000000u;
+      tOffset.nVal *= tXSign.nVal >> 31;
+      result = tOffset.flVal;
+      const float z = y / x;
+      result += (n1 + n2 * z * z) * z;
+    } else {
+      union {
+        float flVal;
+        unsigned int nVal;
+      } tOffset = {PI_2};
+      tOffset.nVal |= tYSign.nVal & 0x80000000u;
+      result = tOffset.flVal;
+      const float z = x / y;
+      result -= (n1 + n2 * z * z) * z;
+    }
+  } else if (y > 0.0f) {
+    result = PI_2;
+  } else if (y < 0.0f) {
+    result = -PI_2;
+  }
+  return result;
+}
+
+template <class T> struct vec3 {
+  T x{};
+  T y{};
+  T z{};
+  vec3 &operator+=(const vec3<T> &o) {
+    x += o.x;
+    y += o.y;
+    z += o.z;
+    return *this;
+  }
 };
 
-template <class VECT> vec3<VECT> operator+(const vec3<VECT>& a, const vec3<VECT>& b) { return { a.x + b.x, a.y + b.y, a.z + b.z }; }
-template <class VECT> vec3<VECT> operator-(const vec3<VECT>& a, const vec3<VECT>& b) { return { a.x - b.x, a.y - b.y, a.z - b.z }; }
-template <class VECT> vec3<VECT> operator*(VECT a, const vec3<VECT>& b) { return { a * b.x, a * b.y, a * b.z }; }
-template <class VECT> vec3<VECT> operator/(const vec3<VECT>& a, VECT b) { return { a.x / b, a.y / b, a.z / b }; }
-template <class VECT> VECT lengthsq(const vec3<VECT>& v) { return v.x * v.x + v.y * v.y + v.z * v.z; }
-vec3<float> unitizef(const vec3<float>& v) { return Q_rsqrt(lengthsq(v)) * v; }
-
-template <class T>
-T cross3(const T& a, const T& b)
-{
-	return {
-		a.y * b.z - a.z * b.y,
-		a.z * b.x - a.x * b.z,
-		a.x * b.y - a.y * b.x,
-	};
+template <class VECT>
+vec3<VECT> operator+(const vec3<VECT> &a, const vec3<VECT> &b) {
+  return {a.x + b.x, a.y + b.y, a.z + b.z};
 }
+template <class VECT>
+vec3<VECT> operator-(const vec3<VECT> &a, const vec3<VECT> &b) {
+  return {a.x - b.x, a.y - b.y, a.z - b.z};
+}
+template <class VECT> vec3<VECT> operator*(VECT a, const vec3<VECT> &b) {
+  return {a * b.x, a * b.y, a * b.z};
+}
+template <class VECT> vec3<VECT> operator/(const vec3<VECT> &a, VECT b) {
+  return {a.x / b, a.y / b, a.z / b};
+}
+template <class VECT> VECT lengthsq(const vec3<VECT> &v) {
+  return v.x * v.x + v.y * v.y + v.z * v.z;
+}
+vec3<float> unitizef(const vec3<float> &v) { return Q_rsqrt(lengthsq(v)) * v; }
 
+template <class T> T cross3(const T &a, const T &b) {
+  return {
+      a.y * b.z - a.z * b.y,
+      a.z * b.x - a.x * b.z,
+      a.x * b.y - a.y * b.x,
+  };
+}
